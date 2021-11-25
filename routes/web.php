9d835a34439e853a92798cdafb0f3350a3fc6be4
Agusioma <terryterrence200@gmail.com>
2021-11-25 15:12:06 +0300

    editweb route

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::get('statPreview/', [StatementsView::class, 'statPreview']);
 Route::get('viewsavings/', [SavingsController::class, 'view_savings']);
 Route::get('totalsavings/', [SavingsController::class, 'total_savings']);
 Route::get('updatedetails/', [AccountController::class, 'update_details']);
+Route::get('updatepassword/', [AccountController::class, 'update_password']);
 
 Route::get('run-migrations', function () {
     try {
