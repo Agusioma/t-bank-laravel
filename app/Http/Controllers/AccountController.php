20d7257c8851889dd957166148e39cf750d5aec9
Agusioma <terryterrence200@gmail.com>
2021-11-25 15:10:21 +0300

    edits to the accounts controller

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -47,4 +47,42 @@ class AccountController extends Controller
           echo($e);
         }
       }
-}
+
+      public function update_password(Request $token)
+      {
+        $receivedData = $token->validate([
+          '_password' => 'required',
+          '_natID' => 'required'
+        ]);
+
+        $updated_fName = $receivedData['_fName'];
+        $updated_lName = $receivedData['_lName'];
+        $updated_eAddress = $receivedData['_eAddress'];
+        $natID = $receivedData['_natID'];
+        $updated_pNumber = $receivedData['_pNumber'];
+       try {
+          //code...
+          $current_user_details = Customer::query()
+          ->where('NatID', $natID)
+          ->get();
+
+          $current_user_details[0]['firstname'] = $updated_fName;
+          $current_user_details[0]['lastname'] = $updated_lName;
+          $current_user_details[0]['email'] = $updated_eAddress;
+          $current_user_details[0]['PhoneNo'] = $updated_pNumber;
+
+          $current_user_details[0]->save();
+
+                $updated_details = Customer::query()
+                ->where('PhoneNo', $updated_pNumber)
+                ->get();
+
+                echo($updated_details);
+
+        } catch (Exception $e) {
+          //throw $th;
+          $e->getMessage();
+          echo($e);
+        }
+      }
+    }
