e756b927518b6e01977523499bb9a3e6126c1d0c
Agusioma <terryterrence200@gmail.com>
2022-02-25 03:24:24 +0300

    daraja building✔

--- app/Http/Controllers/DarajaController.php
+++ app/Http/Controllers/DarajaController.php
@@ -17,7 +17,7 @@ class DarajaController extends Controller
 
         # M-PESA endpoint urls
         $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
- 
+
 
         $curl = curl_init($access_token_url);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -30,5 +30,59 @@ class DarajaController extends Controller
         $access_token = $result->access_token;
         curl_close($curl);
         return $access_token;
+    }
+    public function inititiate_sim(Request $payToken)
+    {
+        date_default_timezone_set('Africa/Nairobi');
+        $receivedData = $payToken->validate([
+            '_amount' => 'required',
+            '_pNumber' => 'required'
+          ]);
+  
+          $amount = $receivedData['_amount'];
+          $pNumber = $receivedData['_pNumber'];
+        # header for stk push
+        $stkheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $this->gen_access_token()];
+        $BusinessShortCode = '174379';
+        $PartyA = $pNumber; // This is your phone number,
+        $AccountReference = 'rr';
+        $TransactionDesc = 'rr';
+        $Amount = $amount;
+        $Timestamp = date('YmdHis');
+        $Passkey = '';
+
+        # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+        $Password = base64_encode($BusinessShortCode . $Passkey . $Timestamp);
+
+        # header for access token
+        $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+        # callback url
+        $CallBackURL = 'https://sacco.terrence-aluda.com/sacco/backend/callback_url.php';
+        # initiating the transaction
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $initiate_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+
+        $curl_post_data = array(
+            //Fill in the request parameters with valid values
+            'BusinessShortCode' => $BusinessShortCode,
+            'Password' => $Password,
+            'Timestamp' => $Timestamp,
+            'TransactionType' => 'CustomerPayBillOnline',
+            'Amount' => $Amount,
+            'PartyA' => $PartyA,
+            'PartyB' => $BusinessShortCode,
+            'PhoneNumber' => $PartyA,
+            'CallBackURL' => $CallBackURL,
+            'AccountReference' => $AccountReference,
+            'TransactionDesc' => $TransactionDesc
+        );
+
+        $data_string = json_encode($curl_post_data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        $curl_response = curl_exec($curl);
     }
 }
